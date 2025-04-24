886958cfaa52695b6df04d584f3d82d4989aece6
Bohdan <bsuhij@gmail.com>
2025-04-24 23:49:49 +0300

    Trying my code

--- app/main.py
+++ app/main.py
@@ -8,10 +8,24 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = [Person(per["name"], per["age"]) for per in people]
-    for index, per in enumerate(people):
-        if per.get("wife"):
-            person_list[index].wife = Person.people[per["wife"]]
-        if per.get("husband"):
-            person_list[index].husband = Person.people[per["husband"]]
-    return person_list
+    persons = []
+
+    for person_info in people:
+        name = person_info["name"]
+        age = person_info["age"]
+        person = Person(name, age)
+        persons.append(person)
+
+    for person_info in people:
+        name = person_info["name"]
+        current_person = Person.people[name]
+
+        if person_info.get("wife"):
+            wife_name = person_info["wife"]
+            current_person.wife = Person.people[wife_name]
+
+        if person_info.get("husband"):
+            husband_name = person_info["husband"]
+            current_person.husband = Person.people[husband_name]
+
+    return persons
