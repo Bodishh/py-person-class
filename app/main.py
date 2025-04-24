6dac8d432d0a21cfee54d772af4bb435f9b4627b
Bohdan <bsuhij@gmail.com>
2025-04-24 23:27:38 +0300

    Task done

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    persons = []
+
+    for person_info in people:
+        name = person_info["name"]
+        age = person_info["age"]
+        person = Person(name, age)
+        persons.append(person)
+
+    for person_info in people:
+        name = person_info["name"]
+        current_person = Person.people[name]
+
+        if person_info.get("wife"):
+            wife_name = person_info["wife"]
+            current_person.wife = Person.people[wife_name]
+
+        if person_info.get("husband"):
+            husband_name = person_info["husband"]
+            current_person.husband = Person.people[husband_name]
+
+    return persons
